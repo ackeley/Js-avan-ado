4fe03ac27677b4cf4f714ef7dda9b90c57f86f07
Ackeley Lennon <ackeley@gmail.com>
2016-08-17 18:10:32 -0300

    adiconando campos a table e pegando os dados para editar

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function setList(lista){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>value</td><td>Action</td></tr></thead><tbody>';
 
     for(key in lista){
-        table += '<tr><td>'+formatDesc(lista[key].desc)+'</td><td>'+lista[key].amount+'</td><td>'+formatValue(lista[key].value)+'</td><td>Edit | Delete</td></tr>';
+        table += '<tr><td>'+formatDesc(lista[key].desc)+'</td><td>'+lista[key].amount+'</td><td>'+formatValue(lista[key].value)+'</td><td><button class="btn btn-warning" onclick="setUpdate('+key+');">Editar</button> | Delete</td></tr>';
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
@@ -30,10 +30,38 @@ function formatDesc(desc){
 }
 
 function formatValue(value){
-    var str =parseFloat(value).toFixed(2) + "";
+    var str = parseFloat(value).toFixed(2) + "";
     str = str.replace(".",",");
-    str = "$ "+str;
+    str = "R$ "+str;
     return str;
+}
+
+function adicionar(){
+    var descricao   = document.getElementById('desc').value;
+    var qtde        = document.getElementById('qtde').value;
+    var valor       = document.getElementById('valor').value;
+
+    lista.unshift({"desc":descricao,"amount":qtde, "value":valor});
+    setList(lista);
+
+}
+
+function setUpdate(id){
+    var obj = lista[id];
+    document.getElementById('desc').value   = obj.desc;
+    document.getElementById('qtde').value   = obj.qtde;
+    document.getElementById('valor').value = obj.valor;
+
+    document.getElementById("btnUpdate").style.display = "inline-block";
+    document.getElementById("btnAdd").style.display = "none";
+}
+
+function resetForm(){
+        document.getElementById('desc').value = "";
+        document.getElementById('qtde').value = "";
+        document.getElementById('valor').value= "";
+        document.getElementById("btnAdd").style.display = "inline-block";
+        document.getElementById("btnUpdate").style.display = "none";
 }
 
 setList(lista);
